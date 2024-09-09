5ece59a18765c463d26a653e094f0e1166051bbe
consuti-tec <consuti@consuti.com.br>
2024-09-08 21:13:39 -0300

    first commit

---
+++ src/AppConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ColorTheme: "#272729",
+  UrlPortal: "https://escoladoesporte.com.br/Vasco",
+  UrlPainel: "https://painel.escoladoesporte.com.br/",
+  TimeSplashScreen: 3000,
+};
